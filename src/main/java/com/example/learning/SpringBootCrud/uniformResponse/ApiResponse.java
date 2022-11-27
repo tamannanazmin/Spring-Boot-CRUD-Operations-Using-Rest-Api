7eeb00bf4f955b3a737b1ff91ac62b0aac16161d
Tamanna Nazmin <42768952+tamannanazmin@users.noreply.github.com>
2022-11-27 17:15:56 +0600

    lombok done

--- src/main/java/com/example/learning/SpringBootCrud/uniformResponse/ApiResponse.java
+++ src/main/java/com/example/learning/SpringBootCrud/uniformResponse/ApiResponse.java
@@ -1,40 +1,14 @@
 package com.example.learning.SpringBootCrud.uniformResponse;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ApiResponse {
     private Integer status;
     private Object data;
     private Object error;
-
-    public ApiResponse() {
-    }
-
-    public ApiResponse(Integer status, Object data, Object error) {
-        this.status = status;
-        this.data = data;
-        this.error = error;
-    }
-
-    public Object getError() {
-        return error;
-    }
-
-    public void setError(Object error) {
-        this.error = error;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    public void setData(Object data) {
-        this.data = data;
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
 }
