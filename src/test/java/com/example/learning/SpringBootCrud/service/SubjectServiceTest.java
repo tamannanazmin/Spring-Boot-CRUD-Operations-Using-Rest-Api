b02027d889d5edf4e88c8d3b8f76d3b6e1eb783e
Tamanna Nazmin <42768952+tamannanazmin@users.noreply.github.com>
2022-12-01 16:42:07 +0600

    unit testing version 1

--- src/test/java/com/example/learning/SpringBootCrud/service/SubjectServiceTest.java
+++ src/test/java/com/example/learning/SpringBootCrud/service/SubjectServiceTest.java
@@ -1,39 +1,85 @@
 package com.example.learning.SpringBootCrud.service;
+import com.example.learning.SpringBootCrud.bean.Subject;
+import com.example.learning.SpringBootCrud.dto.SubjectDto;
 import com.example.learning.SpringBootCrud.repository.SubjectRepository;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 
-//import static jdk.internal.org.objectweb.asm.util.CheckClassAdapter.verify;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+//@ExtendWith(MockitoExtension.class)
 class SubjectServiceTest {
     @Mock
-    private SubjectRepository subjectRepo;
-    private AutoCloseable autoCloseable;
-    private SubjectService underTest;
+    private SubjectRepository subjectRepository;
+    private  SubjectService subjectService;
+    AutoCloseable autoCloseable;
+    @BeforeEach
+    void setUp() {
+        AutoCloseable autoCloseable = MockitoAnnotations.openMocks(this);
 
-//    @BeforeEach
-//    void setUp(){
-//        autoCloseable = MockitoAnnotations.openMocks(this);
-//        underTest = new SubjectService(subjectRepo);
-//    }
+        this.subjectService = new SubjectService(this.subjectRepository);
+    }
 
     @AfterEach
-    void tearDown() throws Exception {
-        autoCloseable.close();
+    void tearDown() {
+        try{
+            this.autoCloseable.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Test
-    void canGetAllSubjects() {
-//        //when
-//        underTest.getAllSubjects();
-//        //then
-//        verify(subjectRepo).findAll();
-//        //
-
+    void getAllSubjects(){
+        subjectService.getAllSubjects();
+        verify(subjectRepository).findAll();
     }
-
+//    @Mock private SubjectRepository subjectRepo;
+//
+//    @InjectMocks
+//    private SubjectService underTest;
+//
+//    @BeforeEach
+//    void setUp(){
+//        //autoCloseable = MockitoAnnotations.openMocks(this);
+//        //underTest = new SubjectService();
+//    }
+////    @AfterEach
+////    void tearDown() throws Exception {
+////        autoCloseable.close();
+////    }
+//
+//    @Test
+//    //@Disabled//this method won't run
+//    void canGetAllSubjects() {
+//        List<Subject> subjects = new ArrayList<>();
+//        Subject subject = new Subject();
+//        subject.setId(String.valueOf(1));
+//        subject.setEmail("abc@gmail.com");
+//        subject.setName("ABC");
+//        subjects.add(subject);
+//
+//        when(subjectRepo.findAll()).thenReturn(subjects);
+//        //when
+//        List<SubjectDto> subjectDtos = underTest.getAllSubjects();
+//        //then
+//        assert subjectDtos.size() == 1;
+//        /*verify(subjectRepo).findAll()
+//                .stream()
+//                .map(SubjectDto::convertEntityToDto)
+//                .collect(Collectors.toList());*/
+//
+//    }
+//
 
     @Test
     @Disabled//this method won't run
